6ffed1fda772f19dcbef62cf56945ba112c2ae4f
Yeho <joaquin_gebauer@hotmail.com>
2017-11-19 13:50:40 -0300

    agregados chunk

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -3,29 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
-	public GameObject[] terrainChunck;
+	public Chunk[] terrainChuncks;
 	public GameObject water;
 
 	public int mapY;
 	public int mapX;
-
-	[Range(0,100)]
-	public float waterProbability;
-
 
 	// Use this for initialization
 	void Start () {
 		generateMap ();
 	}
 	private void generateMap(){
+		GameObject createdChunk = null;
 		for(int x=0; x<mapX;x++){
 			for(int y=0; y<mapY;y++){
-				if (waterProbability > Random.Range (0, 100)) {
-					Instantiate (water, new Vector3 (x, y, 0), Quaternion.identity);
-				} else {
-					Instantiate (terrainChunck[Random.Range(0,terrainChunck.Length)],new Vector3(x,y,0),Quaternion.identity);
+				foreach(Chunk chunk in terrainChuncks){
+					createdChunk = null;
+					if (chunk.pobability > Random.Range (0, 100)) {
+						createdChunk = Instantiate (chunk.terrainchunk, new Vector3 (x, y, 0), Quaternion.identity);
+						createdChunk.transform.parent = transform;
+						break;
+					}
 				}
-
+				if(createdChunk == null){
+					createdChunk = Instantiate (water, new Vector3 (x, y, 0), Quaternion.identity);
+					createdChunk.transform.parent = transform;
+					createdChunk = null;
+				}
 			}
 		}
 	
